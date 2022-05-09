a016584d6c8735a70722ef88f679e6e8d18aac2a
WBomfim <willianbomfim@hotmail.com>
2022-05-09 17:34:01 -0300

    Test: Adicionado testes para os redirecionamentos ao clicar nos links de navegação da aplicação.

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import renderWithRouter from './renderWithRouter';
 import App from '../App';
 
@@ -16,5 +17,48 @@ describe('Testes do componente "App"', () => {
 
       const linkFavorite = screen.getByRole('link', { name: /Favorite Pokémons/i });
       expect(linkFavorite).toBeInTheDocument();
+    });
+
+  it('Verifica o direcionamento para a página inicial ao clicar no link "Home"', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const linkHome = screen.getByRole('link', { name: /Home/i });
+    userEvent.click(linkHome);
+
+    const { location: { pathname } } = history;
+    expect(pathname).toBe('/');
+  });
+
+  it('Verifica o direcionamento para a página About ao clicar no link "About"', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const linkAbout = screen.getByRole('link', { name: /About/i });
+    userEvent.click(linkAbout);
+
+    const { location: { pathname } } = history;
+    expect(pathname).toBe('/about');
+  });
+
+  it('Verifica o direcionamento para a página favoritos ao clicar no link "Favorite"',
+    () => {
+      const { history } = renderWithRouter(<App />);
+
+      const linkFavorite = screen.getByRole('link', { name: /Favorite Pokémons/i });
+      userEvent.click(linkFavorite);
+
+      const { location: { pathname } } = history;
+      expect(pathname).toBe('/favorites');
+    });
+
+  it('Verifica o direcionamento para a página NotFound ao entrar em uma URL desconhecida',
+    () => {
+      const { history } = renderWithRouter(<App />);
+
+      history.push('/NotFound');
+
+      const notFoundExibition = screen.getByRole(
+        'heading', { level: 2, name: /Page requested not found/i },
+      );
+      expect(notFoundExibition).toBeInTheDocument();
     });
 });
