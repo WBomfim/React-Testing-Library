0b98502fefd98d7a3898bc54779c1734aa3adbbc
WBomfim <willianbomfim@hotmail.com>
2022-05-09 17:00:42 -0300

    Test: Criada a função renderWithRouter para renderizar os componentes com rotas e adicionado testes para verificar os links fixos ao renderizar a aplicação.

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,20 @@
-test('VQV!', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+describe('Testes do componente "App"', () => {
+  it('Verifica se o topo da aplicação contém um conjunto fixo de links de navegação',
+    () => {
+      renderWithRouter(<App />);
+
+      const linkHome = screen.getByRole('link', { name: /Hom/i });
+      expect(linkHome).toBeInTheDocument();
+
+      const linkAbout = screen.getByRole('link', { name: /About/i });
+      expect(linkAbout).toBeInTheDocument();
+
+      const linkFavorite = screen.getByRole('link', { name: /Favorite Pokémons/i });
+      expect(linkFavorite).toBeInTheDocument();
+    });
+});
