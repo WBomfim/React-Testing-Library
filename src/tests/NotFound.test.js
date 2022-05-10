6672182731b983a8a0113ac7799a0f410a5db163
WBomfim <willianbomfim@hotmail.com>
2022-05-09 23:24:00 -0300

    Test: Adicionado os testes para o componente NotFound - Os testes verificam o titulo da página e a renderização da imagem correta.

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+describe('Testes do componente "NotFound"', () => {
+  it('Verifica se contém um heading com o texto "Page requested not found 😭"', () => {
+    render(<NotFound />);
+
+    const heading = screen.getByRole(
+      'heading', { level: 2, name: /Page requested not found/i },
+    );
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('Verifica se contém uma imagem com o atributo "src" igual a "https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif"', () => {
+    render(<NotFound />);
+
+    const src = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const nameImg = 'Pikachu crying because the page requested was not found';
+    const image = screen.getByRole('img', { name: nameImg });
+    expect(image.src).toBe(src);
+  });
+});
