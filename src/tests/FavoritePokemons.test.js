59179e23a5f2e5c84c9a158529d15c289841ac4d
WBomfim <willianbomfim@hotmail.com>
2022-05-09 22:10:08 -0300

    Test: Adicionado teste para verificar a exibição do componente "FavoritePokemons" quando não há pokemons favoritados.

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('Testes do componente "FavoritePokemons"', () => {
+  it('Verifica se contém o texto "No favorite pokemon found" quando não há favoritos',
+    () => {
+      render(<FavoritePokemons />);
+
+      const text = screen.getByText(/No favorite pokemon found/i);
+      expect(text).toBeInTheDocument();
+    });
+});
