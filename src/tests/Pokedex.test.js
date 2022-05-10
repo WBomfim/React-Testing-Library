4596940fdc26363cbcf6379f1b82b05a322ca262
WBomfim <willianbomfim@hotmail.com>
2022-05-10 13:06:12 -0300

    Test: Adicionado teste para verificar se o componente "Pokedex" rederiza um h2 com um texto específico.

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+import pokemons from '../data';
+
+describe('Testes do componente "Pokedex', () => {
+  it('Verifica se a página contém um heading h2 com o texto "Encountered pokémons"',
+    () => {
+      renderWithRouter(<App />);
+
+      const heading = screen.getByRole(
+        'heading', { level: 2, name: /Encountered pokémons/i },
+      );
+      expect(heading).toBeInTheDocument();
+    });
+});
