af46d69b55ffa13e15f3c60cb900dbe7435dcb4f
WBomfim <willianbomfim@hotmail.com>
2022-05-10 21:19:18 -0300

    Test: Adicionado testes para verificar a exibição das informações detalhadas de um pokemon.

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,28 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+import pokemons from '../data';
+
+describe('Testes do componente "PokemonDetails"', () => {
+  it('Verifica se as informações detalhadas do pokémon selecionado são mostradas na tela',
+    () => {
+      renderWithRouter(<App />);
+
+      const pokemonDetailsLink = screen.getByRole('link', { name: 'More details' });
+      userEvent.click(pokemonDetailsLink);
+
+      const pokemonDetailsTitle = screen.getByRole(
+        'heading', { name: `${pokemons[0].name} Details` },
+      );
+      expect(pokemonDetailsTitle).toBeInTheDocument();
+      expect(pokemonDetailsLink).not.toBeInTheDocument();
+
+      const detailSumary = screen.getByRole('heading', { name: /Summary/i });
+      expect(detailSumary).toBeInTheDocument();
+
+      const summaryParagraph = screen.getByText(/hard berries with electricity to make/i);
+      expect(summaryParagraph).toBeInTheDocument();
+    });
+});
